2c2f6299dd9eb6b847a243e0144cecaecda6aa59
Yecheng Fu <cofyc.jackson@gmail.com>
2018-03-08 18:49:59 +0800

    Show more infos.

--- list-devices-infos.pl
+++ list-devices-infos.pl
@@ -2,22 +2,36 @@
 #
 # Used to list all scsi devices, and theirs partitions and mountpoints if presented.
 #
+# References:
+# - https://www.kernel.org/pub/linux/utils/util-linux/v2.21/libblkid-docs/libblkid-Partitions-probing.html
+#
 
-my $scsidevices=`lsscsi | awk '{print \$NF}' | grep -P 'sd[a-z]\$'`;
-
+my $scsidevices=`lsscsi | awk '{print \$NF}' | grep -P 'sd[a-z]\$'`; 
 sub print_dev;
 sub print_dev {
     my $dev = shift;
     my $partition = shift;
-    my $output = `blkid -p -s TYPE -s PTTYPE -o export $partition`;
+    my $output = `blkid -p -s TYPE -s PTTYPE -s UUID -s PART_ENTRY_NAME -s PART_ENTRY_UUID -s PART_ENTRY_DISK -o export $partition`;
     my $fstype = "";
     my $pttype = "";
+    my $fsuuid = "";
+    my $pt_entry_name = "";
+    my $pt_entry_uuid = "";
+    my $pt_entry_disk = "";
     for (split(/\n/, $output)) {
         my @seps = split '=', $_, 2;
         if ($seps[0] eq "PTTYPE") {
             $pttype = $seps[1];
         } elsif ($seps[0] eq "TYPE") {
             $fstype = $seps[1];
+        } elsif ($seps[0] eq "UUID") {
+            $fsuuid = $seps[1];
+        } elsif ($seps[0] eq "PART_ENTRY_NAME") {
+            $pt_entry_name = $seps[1];
+        } elsif ($seps[0] eq "PART_ENTRY_UUID") {
+            $pt_entry_uuid = $seps[1];
+        } elsif ($seps[0] eq "PART_ENTRY_DISK") {
+            $pt_entry_disk = $seps[1];
         }
     }
     if ($pttype ne "") {
@@ -31,17 +45,17 @@ sub print_dev {
         if ($output ne "") {
             for (split(/\n/, $output)) {
                 my @seps = split /\s+/, $_;
-                print "$dev\t$partition\t$fstype\t$seps[0]\t$seps[1]\t$seps[2]\t$seps[3]\n";
+                print "$dev\t$partition\t$fsuuid\t$fstype\t$seps[0]\t$seps[1]\t$seps[2]\t$seps[3]\n";
             }
         } else {
-            print "$dev\t$partition\t$fstype\n";
+            print "$dev\t$partition\t$fsuuid\t$fstype\n";
         }
     } else {
         print "error: $dev/$partition does not have parition or filesysem\n"
     }
 }
 
-print "DEVICE\tPARTITION\tFSTYPE\tTARGET\tSOURCE\tFSTYPE\tOPTIONS\n";
+print "DEVICE\tPARTITION\tUUID\tFSTYPE\tTARGET\tSOURCE\tFSTYPE\tOPTIONS\n";
 for my $dev (split(/\n/, $scsidevices)) {
     print_dev($dev, $dev);
 }
