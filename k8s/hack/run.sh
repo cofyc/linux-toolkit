31a56cedb5cc83433b65527d991fa08df9cbd8bc
kirk <kirk-internal@qiniu.com>
2018-08-23 17:46:57 +0800

    Run any script.

--- k8s/hack/run.sh
+++ k8s/hack/run.sh
@@ -10,12 +10,19 @@ source $ROOT/hack/lib.sh
 
 hosts=$(kubectl get nodes -ojsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}')
 
+script=$ROOT/$1
+
+if [ ! -e "$script" ]; then
+    echo "error: $script not found"
+    exit 1
+fi
+
 INVENTORY=output/all.ini
 $ROOT/hack/gen-ansible-inventory.sh > $INVENTORY
 
+src=$script
+dst=/tmp/ansbile.script.$$
 for h in $hosts; do
-    echo "Operating on $h..."
-    #ansible -i $INVENTORY $h -m shell -a 'apt-get update -o Dir::Etc::sourcelist="sources.list.d/kirk.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"; apt-get install kubelet=1.9.7-22+d1c9c68c6b14d2-00'
-    #ansible -i $INVENTORY $h -m shell -a 'docker volume prune -f'
-    ansible -i $INVENTORY $h -m shell -a 'docker volume ls'
+    ansible -i $INVENTORY $h -m copy -a "src=$src dest=$dst"
+    ansible -i $INVENTORY $h -m shell -a "bash $dst"
 done
---
+++ k8s/scripts/upgradekubelet.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+#apt-get update -y
+apt-get update -o Dir::Etc::sourcelist="sources.list.d/kirk.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+apt-get install -y kubelet=1.9.7-41+4e5d7182bd8ac0-00
