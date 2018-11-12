65bd4f4428dd6254505608ae61ac401830a4c894
Yecheng Fu <cofyc.jackson@gmail.com>
2018-11-12 11:35:15 +0800

    move k8s to github.com/cofyc/k8s-toolkit

--- k8s/hack/print-nodes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl get nodes -ogo-template='{{- range .items}}
-   {{- $name := .metadata.name }}
-   {{- range $key, $value := .metadata.labels }}
-       {{- $name }}{{ "\t" }}{{- $key }}: {{ $value }}{{- "\n" }}
-   {{- end }}
-{{- end}}'
